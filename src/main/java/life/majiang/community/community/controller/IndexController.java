a687945fff74d6ac867898af65396afcd69e2fd6
Nj <13241291964@163.com>
2020-01-22 21:19:48 +0800

    add interceptor

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -2,18 +2,12 @@ package life.majiang.community.community.controller;
 
 import life.majiang.community.community.Service.QuestionService;
 import life.majiang.community.community.dto.PageinationDto;
-import life.majiang.community.community.dto.QuestionDto;
 import life.majiang.community.community.mapper.UserMapper;
-import life.majiang.community.community.mode.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 
 @Controller
@@ -25,22 +19,9 @@ public class IndexController {
     QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
                         @RequestParam(name = "size",defaultValue = "5") Integer size){
-        Cookie[] cookies = request.getCookies();
-        if(cookies!=null && cookies.length!=0){
-            for (Cookie cookie : cookies) {
-                if(cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if(user!=null)
-                        request.getSession().setAttribute("user",user);
-                    break;
-                }
-            }
-        }
         //列表数据获取
         PageinationDto pageination = questionService.list(page,size);
         model.addAttribute("pageination",pageination);
--- src/main/java/life/majiang/community/community/controller/ProfileController.java
+++ src/main/java/life/majiang/community/community/controller/ProfileController.java
@@ -10,8 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -27,20 +25,7 @@ public class ProfileController {
                                Model model,
                                @RequestParam(name = "page",defaultValue = "1") Integer page,
                                @RequestParam(name = "size",defaultValue = "5") Integer size){
-            //验证登录状态
-            Cookie[] cookies = request.getCookies();
-            User user=null;
-            if(cookies!=null && cookies.length!=0){
-                for (Cookie cookie : cookies) {
-                    if(cookie.getName().equals("token")) {
-                        String token = cookie.getValue();
-                        user = userMapper.findByToken(token);
-                        if(user!=null)
-                            request.getSession().setAttribute("user",user);
-                        break;
-                    }
-                }
-            }
+            User user = (User) request.getSession().getAttribute("user");
             if (user==null){
                 return "redirect:/";
             }
